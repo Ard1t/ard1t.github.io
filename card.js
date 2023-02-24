dfa1360973303426677ac067ac44540f3efba077
Ard1t <ardit.dani@gmail.com>
2023-02-25 00:49:10 +0100

    adding cards file again

--- card.js
+++ card.js
@@ -1,26 +1,26 @@
 export default {
-  coverImage: 'https://i.pinimg.com/originals/5e/66/18/5e6618a423dbc370f695a947842724ef.jpg',
+  coverImage: 'https://raw.githubusercontent.com/Ard1t/ard1t.github.io/main/images/hardware.jpg',
   profileImage: 'https://avatars.githubusercontent.com/u/1041415?v=4',
   fullName: 'Ardit Dani',
-  profession: 'Decom Data Tech',
+  profession: 'Data Tech L3',
   cv: {
     url: 'http://www.msnlabs.com/img/resume-sample.pdf',
     icon: 'mdi-download'
   },
   email: {
-    address: 'arditdani@gmail.com',
+    address: 'ardit.dani@gmail.com',
     icon: 'mdi-email-send'
   },
   links: [
     {
       title: 'Github',
-      url: '#',
+      url: 'https://github.com/Ard1t',
       icon: 'mdi-github'
     },
     {
       title: 'Linkedin',
-      url: '#',
+      url: 'https://linkedin.com/in/ardit-dani',
       icon: 'mdi-linkedin'
     },
-   ]
+  ]
 };
