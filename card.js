ebbe3177f6c9b0c7e3cb3fb550d9a594ff745b10
Ard1t <ardit.dani@gmail.com>
2023-02-25 00:41:20 +0100

    adding cards file

---
+++ card.js
@@ -0,0 +1,26 @@
+export default {
+  coverImage: 'https://i.pinimg.com/originals/5e/66/18/5e6618a423dbc370f695a947842724ef.jpg',
+  profileImage: 'https://avatars.githubusercontent.com/u/1041415?v=4',
+  fullName: 'Ardit Dani',
+  profession: 'Decom Data Tech',
+  cv: {
+    url: 'http://www.msnlabs.com/img/resume-sample.pdf',
+    icon: 'mdi-download'
+  },
+  email: {
+    address: 'arditdani@gmail.com',
+    icon: 'mdi-email-send'
+  },
+  links: [
+    {
+      title: 'Github',
+      url: '#',
+      icon: 'mdi-github'
+    },
+    {
+      title: 'Linkedin',
+      url: '#',
+      icon: 'mdi-linkedin'
+    },
+   ]
+};
